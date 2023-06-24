f5698e7a46fcebc9c8dbede638348a9af2a6171f
Paula Rusti <rusti.paula78@gmail.com>
2023-06-24 18:39:07 +0300

    Implement insight page

--- src/views/owner/InsightPage.vue
+++ src/views/owner/InsightPage.vue
@@ -3,8 +3,8 @@
     <v-responsive>
           <v-row>
             <v-col cols="12">
-              <h1>Reservations Insights</h1>
-              <p>On this page you can see reservations insights in general and per property.</p>
+              <h1>Overall Insights</h1>
+              <p>On this page you can see overall insights in general and per property.</p>
               <br>
             </v-col>
             <v-col cols="6">
@@ -16,10 +16,10 @@
           </v-row>
           <v-row>
             <v-col>
-              <h1>Revenue Insights</h1>
-              <p>On this page you can see revenue insights in general and per property.</p>
+              <h1>Reservations Insights</h1>
+              <p>On this page you can see reservation insights in general and per property.</p>
               <br>
-              <highcharts class="hc" :options="revenueChartOptions" ref="revenueChart"></highcharts>
+              <highcharts class="hc" :options="reservationsPerDayChartOptions" ref="revenueChart"></highcharts>
               <br>
             </v-col>
           </v-row>
@@ -28,18 +28,127 @@
 </template>
 
 <script>
+import {useBookingsStore} from "@/store/bookings";
+
 export default {
   name: "InsightPage",
+  watch: {
+    "bookingsStore.bookings": {
+      handler() {
+        this.reservationsChartOptions.series[0].data = this.topReservations;
+        this.topRevenueChartOptions.series[0].data = this.topRevenue;
+        this.reservationsPerDayChartOptions.series = this.bookingsStatistics;
+      }
+    }
+  },
+  setup() {
+    let bookingsStore = useBookingsStore();
+    return {
+      bookingsStore: bookingsStore,
+    }
+  },
+  mounted() {
+    this.bookingsStore.retrieveBookingsOfOwner();
+  },
+  computed: {
+    bookingsStatistics() {
+      let data = []
+
+      let countReservationsByDay = {};
+      this.bookingsStore.bookings.forEach(booking => {
+        let date = new Date(booking.start_date);
+        let cabinName = booking.cabin_name.name;
+        date.setHours(0, 0, 0, 0);
+        if (countReservationsByDay[cabinName] === undefined) {
+          // date is a timestamp
+          countReservationsByDay[cabinName] = {
+            x: date.getTime(),
+            y: 1,
+          }
+        } else {
+          countReservationsByDay[cabinName] = {
+            x: date.getTime(),
+            y: countReservationsByDay[cabinName].y + 1,
+          }
+        }
+      });
+
+      let reservationsPerDay = {};
+      for (const [key, value] of Object.entries(countReservationsByDay)) {
+        if (reservationsPerDay[key] === undefined) {
+          reservationsPerDay[key] = [[value.x, value.y]];
+        } else {
+          reservationsPerDay[key].push([value.x, value.y]);
+        }
+      }
+
+      for (const [key, value] of Object.entries(reservationsPerDay)) {
+        data.push({
+          name: key,
+          data: value,
+        })
+      }
+
+      return data;
+    },
+    topRevenue() {
+      let totalRevenue = this.bookingsStore.bookings.reduce((acc, booking) => {
+        return acc + booking.price;
+      }, 0);
+      let cabins = {};
+
+      this.bookingsStore.bookings.forEach(booking => {
+        if (cabins[booking.cabin_name.name] === undefined) {
+          cabins[booking.cabin_name.name] = booking.price;
+        } else {
+          cabins[booking.cabin_name.name] += booking.price;
+        }
+      });
+
+      let revenue = []
+
+      for (const [key, value] of Object.entries(cabins)) {
+        revenue.push({
+          name: key,
+          y: value / totalRevenue * 100,
+        })
+      }
+
+      return revenue;
+    },
+    topReservations() {
+      let totalBookings = this.bookingsStore.bookings.length;
+      let cabins = {};
+
+      this.bookingsStore.bookings.forEach(booking => {
+        if (cabins[booking.cabin_name.name] === undefined) {
+          cabins[booking.cabin_name.name] = 1;
+        } else {
+          cabins[booking.cabin_name.name] += 1;
+        }
+      });
+
+      let reservations = []
+      for (const [key, value] of Object.entries(cabins)) {
+        reservations.push({
+          name: key,
+          y: value / totalBookings * 100,
+        })
+      }
+
+      return reservations;
+    }
+  },
   data() {
     return {
-      revenueChartOptions: {
+      reservationsPerDayChartOptions: {
         title: {
-          text: 'Revenue',
+          text: 'Reservations per day',
           align: 'left'
         },
 
         subtitle: {
-          text: 'The amount of revenue per week',
+          text: 'The amount of reservations per day in your cabins',
           align: 'left'
         },
         xAxis: {
@@ -96,13 +205,7 @@ export default {
           name: 'Cabin',
           colorByPoint: true,
           type: "pie",
-          data: [{
-            name: 'Cabana Florilor',
-            y: 70.67,
-          }, {
-            name: 'Casa Flori de Nuc',
-            y: 30.33
-          }]
+          data: []
         }]
       },
       topRevenueChartOptions: {
